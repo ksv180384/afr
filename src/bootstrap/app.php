f456ba9af1ef1df76642de8eb963c2dd960c6953
s.kotelevsky <s.kotelevsky@sfbtech.org>
2026-04-06 18:49:44 +0300

    Правки сео

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -18,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
-            \App\Http\Middleware\PrerenderIfCrawler::class,
+//            \App\Http\Middleware\PrerenderIfCrawler::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
             \App\Http\Middleware\TrackRefererMiddleware::class,
